37bc0dc276819573860729f8a75f22c3c54c31a9
tribecon1 <bentl24@byu.edu>
2024-05-25 15:55:16 -0600

    First Service Test written, positive case for `register` method

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,6 +1,16 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.dao.AuthDao;
+import dataaccess.dao.UserDao;
+import dataaccess.dao.memoryDao.MemoryAuthDao;
+import dataaccess.dao.memoryDao.MemoryUserDao;
+import model.AuthData;
 import model.UserData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import response.ResponseType;
 
 
 public class UserServiceTests {
@@ -9,6 +19,27 @@ public class UserServiceTests {
 
     private static UserData newUser;
     private static UserData currUser;
+
+    private static String logoutAuthToken;
+
+
+    @BeforeAll
+    public static void setUp() {
+        UserDao givenUserDao = new MemoryUserDao();
+        AuthDao givenAuthDao = new MemoryAuthDao();
+
+        userService = new UserService(givenUserDao, givenAuthDao);
+    }
+
+
+    @Test
+    public void registerSuccessTest() throws DataAccessException {
+        UserData newUser = new UserData("MyTestUser","Pass123", "t@gmail.com");
+        ResponseType response = userService.register(newUser);
+        Assertions.assertNotNull(response);
+        assert (response instanceof AuthData);
+
+    }
 
 
 
