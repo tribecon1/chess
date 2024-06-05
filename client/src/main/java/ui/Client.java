a793f95306f251cee5683f758ff4a80db1e7118a
tribecon1 <bentl24@byu.edu>
2024-06-05 00:09:36 -0600

    Client Start-Up Menu is looping based off of bad inputs, help commands, or proceeds w/ register or login

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,6 +1,9 @@
 package ui;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
@@ -10,6 +13,9 @@ public class Client {
     private static final PrintStream OUT = System.out;
     private static final Scanner TERMINAL_READER = new Scanner(System.in);
     private static String authToken = null;
+    private static final List<String> STARTUP_COMMANDS = new ArrayList<>(Arrays.asList("REGISTER", "LOGIN", "QUIT"));
+    private static boolean badCommand = false;
+    private static boolean helpLoop = false;
 
     public static void main(String[] args){
         while (authToken == null){
@@ -27,46 +33,80 @@ public class Client {
         OUT.println("♖ Welcome to Bentley's CS 240 Chess Fest! ♜");
         OUT.println("***********************************************************************************");
         OUT.println();
-        OUT.println("Type \"help\" to see your options, or enter your desired action here!:");
-        String userResponse = TERMINAL_READER.nextLine().toUpperCase();
-        switch(userResponse){
-            case "HELP":
-                helpMenuOptions();
-                break;
-            case "LOGIN":
-                OUT.println("Login code here!");
-                break;
-            case "REGISTER":
-                OUT.println("Register code here!");
-                break;
-            case "QUIT":
-                OUT.println("Closing your connection to the Chess Server...");
-                System.exit(0);
-        }
+        OUT.print(SET_TEXT_COLOR_WHITE);
 
+        while(true){
+            OUT.println("Type \"help\" to see your options, or enter your desired action here!:");
+
+            String userResponse = TERMINAL_READER.nextLine().toUpperCase();
+
+            switch(userResponse){
+                case "HELP":
+                    helpMenuOptions();
+                    break;
+                case "REGISTER":
+                    registerInfoSteps();
+                    break;
+                case "LOGIN":
+                    OUT.println("Login code here!");
+                    break;
+                case "QUIT":
+                    OUT.println("Closing your connection to the Chess Server...");
+                    TERMINAL_READER.close();
+                    System.exit(0);
+                default:
+                OUT.println("ERROR! Unknown command: " + "\"" + userResponse + "\"");
+            }
+        }
     }
+
 
 
     private static void helpMenuOptions(){
         OUT.println("Here are the following commands: ");
-        OUT.print(SET_TEXT_COLOR_BLUE);
-        OUT.print("   1. Register - Provide a ");
+        OUT.print(SET_TEXT_COLOR_GREEN);
+        OUT.print("   * Register - Provide a ");
         OUT.print(SET_TEXT_COLOR_ORANGE);
         OUT.print("<USERNAME> <PASSWORD> <EMAIL> ");
-        OUT.print(SET_TEXT_COLOR_BLUE);
+        OUT.print(SET_TEXT_COLOR_GREEN);
         OUT.print("to register a new user!");
         OUT.println();
 
-        OUT.print("   2. Login - Provide an existing ");
+        OUT.print(SET_TEXT_COLOR_BLUE);
+        OUT.print("   * Login - Provide an existing ");
         OUT.print(SET_TEXT_COLOR_ORANGE);
         OUT.print("<USERNAME> <PASSWORD> ");
         OUT.print(SET_TEXT_COLOR_BLUE);
         OUT.print("to login!");
         OUT.println();
 
+        OUT.print(SET_TEXT_COLOR_MAGENTA);
+        OUT.println("   * Help - List of available commands");
+
         OUT.print(SET_TEXT_COLOR_RED);
-        OUT.println("   3. Quit your current session");
+        OUT.println("   * Quit your current session");
         OUT.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void registerInfoSteps(){
+        OUT.println("Create your username:");
+        String userResponse = TERMINAL_READER.nextLine();
+        while(userResponse.isEmpty()){
+            OUT.println("Username may not be blank, please try again: ");
+            userResponse = TERMINAL_READER.nextLine();
+        }
+        OUT.println("Create your password:");
+        userResponse = TERMINAL_READER.nextLine();
+        while(userResponse.isEmpty()){
+            OUT.println("Password may not be blank, please try again: ");
+            userResponse = TERMINAL_READER.nextLine();
+        }
+        OUT.println("Enter your email:");
+        userResponse = TERMINAL_READER.nextLine();
+        while(userResponse.isEmpty()){
+            OUT.println("Email may not be blank, please try again:");
+            userResponse = TERMINAL_READER.nextLine();
+        }
     }
 
 }
